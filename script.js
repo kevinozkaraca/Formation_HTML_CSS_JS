3e0d06ed1c82b7cacd4e1479cc0738a41b2a1406
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2022-10-30 11:20:57 +0300

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-console.log('test en console pour vérifier la connexion au  fichier html :');
-console.log('Ho yeah !');
+console.log("test en console pour vérifier la connexion au  fichier html :");
+console.log("Ho yeah !");
 
 // commentaire monoligne
 /*
@@ -14,10 +14,10 @@ lignes
 let maVariable1 = 1;
 const maVariable2 = 2;
 var maVariable3 = 3;
-console.log('-- Variable déclarée à = 1 -- resultat :');
+console.log("-- Variable déclarée à = 1 -- resultat :");
 console.log(maVariable1);
 // changer une valeur pas besoin de let ou var (impossble pour const) :
-console.log('-- Variable modifiée à = 3 -- resultat :');
+console.log("-- Variable modifiée à = 3 -- resultat :");
 maVariable1 = 3;
 console.log(maVariable1);
 
@@ -38,9 +38,9 @@ let maVariableNumber2 = 123.123;
 // BigInt (trop grand pour etre représenté)
 let maVariableBigInt = BigInt(9007199254740991);
 // String
-let maVariableString = '123';
-let maVariableStringPX = '123px';
-let maVariableStringVirgule = '123.123';
+let maVariableString = "123";
+let maVariableStringPX = "123px";
+let maVariableStringVirgule = "123.123";
 
 // 123 n'est pas équivalent à "123"
 // 123 + "123" = "123132"
@@ -66,9 +66,9 @@ console.log(Number(maVariableUndefined));
 console.log("-- Convertion d'un true en string :");
 console.log(String(maVariableTrue));
 console.log(maVariableNumber1.toString());
-console.log('-- Recupération des number dans un string (123px) : ');
+console.log("-- Recupération des number dans un string (123px) : ");
 console.log(parseInt(maVariableStringPX));
-console.log('-- couper un number à la virgule (123.123) : ');
+console.log("-- couper un number à la virgule (123.123) : ");
 console.log(parseInt(maVariableNumber2));
 console.log("-- convertir un string en number à la virgule '123.123' : ");
 console.log(parseFloat(maVariableStringVirgule));
@@ -76,7 +76,7 @@ console.log(parseFloat(maVariableStringVirgule));
             ---LES KEYWORDS - mots réservé à javascript---
 */
 //Exemple
-console.log('-- Exemple de keywords en javascript : ');
+console.log("-- Exemple de keywords en javascript : ");
 console.log(`
 await	
 break	
@@ -129,30 +129,30 @@ yield
 /*
             ---OPERATEURS MATHEMATIQUE---
 */
-console.log('Calcul simple avec +, -, /, * et ** ');
-console.log('10 + 10 :');
+console.log("Calcul simple avec +, -, /, * et ** ");
+console.log("10 + 10 :");
 console.log(10 + 10);
-console.log('10 - 10 :');
+console.log("10 - 10 :");
 console.log(10 - 10);
-console.log('100 ** 10 :');
+console.log("100 ** 10 :");
 console.log(100 / 10);
-console.log('10 * 10 :');
+console.log("10 * 10 :");
 console.log(10 * 10);
-console.log('10 ** 10 :');
+console.log("10 ** 10 :");
 console.log(10 ** 10);
 /*
             ---ASSIGNEMENT---
 */
 let maVariable10 = 10;
-console.log('10 += 1 :');
+console.log("10 += 1 :");
 console.log((maVariable10 += 1));
-console.log('10 -= 10 :');
+console.log("10 -= 10 :");
 console.log((maVariable10 -= 10));
-console.log('10 *= :');
+console.log("10 *= :");
 console.log((maVariable10 *= 100));
-console.log('++100');
+console.log("++100");
 console.log(++maVariable10);
-console.log('--101');
+console.log("--101");
 console.log(--maVariable10);
 /*
             ---LES CONDITIONS---
@@ -160,12 +160,13 @@ console.log(--maVariable10);
 let maCondition1 = true;
 let maCondition2 = 4;
 let maCondition3 = 2;
-console.log('Condition valable ? : ');
+console.log("Condition valable ? : ");
 if (maCondition1 == true) {
   console.log("It's true");
 }
 // si ma condition n'est pas plus petit que 3
 // < > <= >= == ===
+
 if (!maCondition2 < 3) {
   console.log("It's true");
 }
@@ -177,3 +178,13 @@ if (!maCondition2 < 3 || maCondition3 === 2) {
 }
 
 // Les maths avant la comparaison
+
+// concaténation :
+console.log(`avec les ${maVariable10 + maCondition1} et les ${maVariableString}`);
+// opération dans une variable
+let maVariable4 = maVariable10 + maCondition1;
+console.log(maVariable4);
+
+/*
+            ---LES STRUCTURES CONDITIONNELLES ---
+*/
