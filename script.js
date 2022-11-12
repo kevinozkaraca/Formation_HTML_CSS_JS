1dde31fbbdc992ded3b74c13a37198ec71b8e5a6
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2022-11-12 17:27:23 +0300

    first12313215454

--- script.js
+++ script.js
@@ -458,7 +458,22 @@ console.log(map1);
 /*
             --- Les fonctions sur les chaines de caractères ---
 */
-let chaineDeCara = "hdklfhdsjkfgdjkmsqghouidqmshbvlkxwcbvkjuzeatgeiudhbvcjklhdbsqgjkfhsdqkljhflkqjsdhgfo";
+let chaineDeCara = "     +hdklfhdsjkfgdjk msqghouidq+mshbvlkxwcbvkjuzeatge iudhbvcjklhdbsqg k+fhsdqkljhflkqjsdhg+fo";
 // les chaines de caractère peuvent se travailler comme un tableau :
 let bouDeChaine = chaineDeCara[2];
 console.log(bouDeChaine);
+console.log(chaineDeCara.indexOf("r")); // -1
+console.log(chaineDeCara.indexOf("o")); // 20 eme
+console.log(chaineDeCara.slice(25)); // 83
+console.log(chaineDeCara.slice(25, 70)); // de 25 à 70
+console.log(chaineDeCara.slice(-10)); // a partir de la fin
+console.log(chaineDeCara.trim()); // supprime les vides avant
+console.log(chaineDeCara.toLocaleLowerCase());
+console.log(chaineDeCara.toUpperCase());
+console.log(chaineDeCara.replace("", "a")); // remplace 1 fois seulement
+console.log(chaineDeCara.replace(/h/g, "REMPLACE")); // regex, lettres remplace les h par "REMPLACE"
+console.log(chaineDeCara.includes("a")); // true si a si trouve
+console.log(chaineDeCara.split("+")); // fait un tableau a chaque +
+/*
+            --- La boucle for ---
+*/
