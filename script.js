82ea4a995e0fb23358ba332fcaf1163091308783
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2022-11-13 00:17:07 +0300

    first12313215454

--- script.js
+++ script.js
@@ -535,3 +535,21 @@ console.log(Math.round(25.5)); //arrondie
 console.log(Math.floor(23.9)); //arrondie en dessous (comme trunc en positif mais pas en negatif)
 console.log(Math.trunc(Math.random() * 5) + 1); //(nombre aléatoire de 0 à 5 tronqué plus 1 (changé 5 en cas de besoin)
 console.log(monExemple.toFixed(2)); //arrondi a deux chiffres apres la virgule
+/*
+            --- Les dates ---
+*/
+const date = new Date();
+console.log(date);
+console.log(new Date(2035, 10, 19, 15, 23, 5)); //resultat :  nov  13 2037 15 :23 :05
+console.log(date.getYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+/*
+            --- Modifier le DOM ---
+*/
+// variable = document.selecteur(...)
+// variable.transformation
