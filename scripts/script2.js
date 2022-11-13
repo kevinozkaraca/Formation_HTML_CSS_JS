8a8ce97ddbd84cae643cce079e60fd73b54bd1e0
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2022-11-13 11:30:09 +0300

    first12313215454

--- scripts/script2.js
+++ scripts/script2.js
@@ -3,3 +3,34 @@
 */
 // variable = document.selecteur(...)
 // variable.transformation
+
+// selecteur
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+let button4 = document.getElementById("button4");
+let button5 = document.getElementById("button5");
+
+// Evenement
+button1.addEventListener("click", button1function);
+button2.addEventListener("click", button2function);
+button3.addEventListener("click", button3function);
+button4.addEventListener("click", button1function);
+button5.addEventListener("click", button1function);
+
+// fonction du
+function button1function() {
+  console.log("kdhflkdh");
+}
+function button2function() {
+  console.log("kdhflkdh");
+}
+function button3function() {
+  console.log("kdhflkdh");
+}
+function button4function() {
+  console.log("kdhflkdh");
+}
+function button5function() {
+  console.log("kdhflkdh");
+}
